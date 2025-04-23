48ebc1f9d61fa00f90d8bab11743261c62025ecf
vlad-radilla <vlad.radilla@gmail.com>
2025-04-23 13:27:05 -0600

    Primer commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Montserrat, Lusitana } from 'next/font/google';
+
+export const montserrat = Montserrat({
+  subsets: ['latin']
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
